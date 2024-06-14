30e11113d1d361d5f3269640cb0142c88d68a873
Alefe Alves <alefealves@hotmail.com>
2024-06-14 11:38:06 -0400

    feat: add models

--- src/PostManager.Domain/Common/Models/ValueObject.cs
+++ src/PostManager.Domain/Common/Models/ValueObject.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace PostManager.Domain.Common.Models
 {
-    internal class ValueObject
+    public abstract class ValueObject : IEquatable<ValueObject>
     {
+        public abstract IEnumerable<object?> GetEqualityComponents();
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null || obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            var valueObject = (ValueObject)obj;
+
+            return GetEqualityComponents()
+                .SequenceEqual(valueObject.GetEqualityComponents());
+        }
+
+        public static bool operator ==(ValueObject left, ValueObject right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(ValueObject left, ValueObject right)
+        {
+            return !Equals(left, right);
+        }
+
+        public override int GetHashCode()
+        {
+            return GetEqualityComponents()
+                .Select(x => x?.GetHashCode() ?? 0)
+                .Aggregate((x, y) => x ^ y);
+        }
+
+        public bool Equals(ValueObject? other)
+        {
+            return Equals((object?)other);
+        }
     }
 }
--- src/PostoManager.Api/obj/Debug/net8.0/PostoManager.Api.AssemblyInfo.cs
+++ src/PostoManager.Api/obj/Debug/net8.0/PostoManager.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PostoManager.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fc8ea6bf649387fed407a28a4a8d625a9578373")]
 [assembly: System.Reflection.AssemblyProductAttribute("PostoManager.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PostoManager.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
